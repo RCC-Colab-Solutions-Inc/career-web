ad8d850c40f676a8dcffa5e4b0acc5baa126942f
itsmekentoy <kent.troubleshooter@gmail.com>
2025-03-15 11:42:53 +0800

    add model and migration for JobPosting

---
+++ app/Models/JobPosting.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JobPosting extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'jobtitle',
+        'jobdescription',
+        'jobtypes',
+        'joblocation',
+        'jobstatus',
+        'others'
+    ];
+
+    public function jobdescription()
+    {
+        return $this->hasOne(jobdescription::class);
+    }
+}
---
+++ database/migrations/2025_03_15_025751_create_job_postings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_postings', function (Blueprint $table) {
+            $table->id();
+            $table->string('jobcode');
+            $table->string('jobtitle');
+            $table->string('jobdescription');
+            $table->enum('workplace',['On-Site', 'Remote', 'Hybrid']);
+            $table->string('joblocation');
+            $table->string('jobtype');
+            $table->string('department');
+            $table->enum('jobstatus', ['open', 'closed']);
+            $table->enum('joburgency', ['urgent', 'normal']);
+            $table->text('others');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_postings');
+    }
+};
