8a35e8c7a4f1a9f4fe55ab78237622d8b10471c3
itsmekentoy <kent.troubleshooter@gmail.com>
2025-03-15 22:32:43 +0800

    register api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\ApiAuthMiddleware;
-use App\Http\Middleware\LoginAuthorization;
+use App\Http\Middleware\LoginMainController;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
