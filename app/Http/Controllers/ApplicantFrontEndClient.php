70adf4b3929489794e20322cabe223fa78b5de0b
itsmekentoy <kent.troubleshooter@gmail.com>
2025-04-14 08:09:46 +0800

    frontend for cleint

---
+++ app/Http/Controllers/ApplicantFrontEndClient.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\ApplicantsApplication;
+use App\Models\JobPosting;
+use App\Models\CompanyDatabase;
+use App\Models\ApplicantStatus;
+use Devrabiul\ToastMagic\Facades\ToastMagic;
+use Illuminate\Http\Request;
+
+class ApplicantFrontEndClient extends Controller
+{
+    //
+}
