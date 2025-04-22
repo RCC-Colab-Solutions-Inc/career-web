6835054931fe208e58802b0a827ea1b274e94bcc
itsmekentoy <kent.troubleshooter@gmail.com>
2025-04-22 23:33:52 +0800

    c

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->statefulApi();
         $middleware->validateCsrfTokens(except: [
             '/logins',
+            '/dashboard'
             '/logout',
         ]);
     })
