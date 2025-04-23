4a4b5e04db84ee0ec591b360221af9ba9ae2d307
itsmekentoy <kent.troubleshooter@gmail.com>
2025-04-23 09:40:26 +0800

    add test

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('SANCTUM_STATEFUL_DOMAINS'),'http://localhost:5174'],
+    'allowed_origins' => [env('SANCTUM_STATEFUL_DOMAINS'),'http://localhost:5174','https://test.rcccolabsolutions.com'],
 
     'allowed_origins_patterns' => [],
 
