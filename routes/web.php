6ec1f440fab572bbaeb2bfa0e8e9a8f52342ea02
JustineKennethGaviola <kenneth.gaviola@rcccolabsolutions.com>
2025-04-24 12:44:00 +0800

    Make the url clipboard functional

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,15 @@ use Illuminate\Support\Facades\Auth;
 //     return view('dashboard');
 // });
 
-
+Route::get('/job/{jobcode}', function ($jobcode) {
+    $job = App\Models\JobPosting::where('jobcode', $jobcode)->first();
+    
+    if ($job) {
+        return redirect()->away('http://localhost:5174/career?job=' . $jobcode);
+    } else {
+        abort(404);
+    }
+});
     
 
 
