92740c98ef5e2be27b1a9c21a2ba56b820f3ff11
itsmekentoy <kent.troubleshooter@gmail.com>
2025-04-24 17:46:32 +0800

    revert

--- app/Http/Controllers/ClientMainController.php
+++ app/Http/Controllers/ClientMainController.php
@@ -90,17 +90,6 @@ class ClientMainController extends Controller
             ->select('applicants_applications.*','job_postings.jobtitle')
 
             ->get();
-
-        //select all status of the applicant
-        $status  = ApplicantStatus::where('applicant_id', $applicants->id)->get();
-        //check if the applicant is empty
-        if ($status->isEmpty()) {
-           $stat = null;
-        }else{
-            $stat = $status;
-        }
-
-
         
         if ($applicants->isEmpty()) {
             return response()->json([
@@ -115,7 +104,6 @@ class ClientMainController extends Controller
             'message_tokenized' => 'Token is valid',
             'code' => 200,
             'applicants' => $applicants,
-            'statuses' => $stat,
         ]);
 
 
