35ee6d1be6730cfa2122d9b862b168455fb9457e
itsmekentoy <kent.troubleshooter@gmail.com>
2025-04-25 09:04:32 +0800

    unset-s-frame

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\ApiAuthMiddleware;
 use App\Http\Middleware\LoginMainController;
 use App\Http\Middleware\TokenizedCompany;
+use App\Http\Middleware\XFrameHeadersMiddleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -17,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'api.auth' => ApiAuthMiddleware::class,
+            'x-frame' => XFrameHeadersMiddleware::class,
         ]);
        
         $middleware->statefulApi();
